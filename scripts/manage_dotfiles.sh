9813e2c81700da1ce3517f2822a9347510f06201
AbdelhakimMhioul <hk.mhl1975@gmail.com>
2022-07-07 12:53:57 +0100

    feat: :sparkles: Crow Translate config file added

--- scripts/manage_dotfiles.sh
+++ scripts/manage_dotfiles.sh
@@ -3,4 +3,5 @@
 # Add Dotfiles (Folders)
 rm -rf $HOME/.config/autostart && ln -s $HOME/Templates/code-utilities/dotfiles/autostart $HOME/.config/autostart
 # Add Dotfiles (Files)
+rm -rf $HOME/.config/Crow\ Translate/Crow\ Translate.conf && ln -s $HOME/Templates/code-utilities/dotfiles/Crow\ Translate.conf $HOME/.config/Crow\ Translate/Crow\ Translate.conf
 rm -rf $HOME/.cargo/config.toml && ln -s $HOME/Templates/code-utilities/dotfiles/cargo/config.toml $HOME/.cargo/config.toml
