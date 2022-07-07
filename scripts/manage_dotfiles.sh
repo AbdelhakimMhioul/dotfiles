c22f4afb7ebc3b487efb8001310523a99918daf0
AbdelhakimMhioul <hk.mhl1975@gmail.com>
2022-07-07 12:49:06 +0100

    feat: :art: Rust cargo config added

--- scripts/manage_dotfiles.sh
+++ scripts/manage_dotfiles.sh
@@ -2,3 +2,5 @@
 
 # Add Dotfiles (Folders)
 rm -rf $HOME/.config/autostart && ln -s $HOME/Templates/code-utilities/dotfiles/autostart $HOME/.config/autostart
+# Add Dotfiles (Files)
+rm -rf $HOME/.cargo/config.toml && ln -s $HOME/Templates/code-utilities/dotfiles/cargo/config.toml $HOME/.cargo/config.toml
